3435ae536f63cddc851f51725bda3c993e4f946d
aboodjabr0 <jabr.abood@yahoo.com>
2025-08-26 10:27:33 +0300

    read_file_update

--- get_next_line.c
+++ get_next_line.c
@@ -11,18 +11,54 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#define BUFF_SIZE 1024
 
-char	read_file(int fd, char *buffer)
+char *ft_join(char *full_file, char *s_chunk)
 {
+    char *tmp;
 
+    tmp = ft_strjoin(full_file, s_chunk);
+    free (full_file);
+    return(tmp);
+}
+char *read_file(int fd, char *full_file)
+{
+    char *s_chunk;
+    int bytes_read;
+
+    if (!full_file)
+        full_file = ft_calloc(1, 1);
+    s_chunk = ft_calloc(BUFF_SIZE + 1, sizeof(char));
+    bytes_read = 1;
+    while (bytes_read > 0)
+    {
+        bytes_read = read(fd, s_chunk, BUFF_SIZE);
+        if (bytes_read == -1)
+        {
+            free(s_chunk);
+            return (NULL);
+        }
+        s_chunk[bytes_read] = '\0';
+        full_file = ft_join(full_file, s_chunk);
+        if (ft_strchr(full_file, '\n'))
+            break;
+    }
+    free(s_chunk);
+    return (full_file);
 }
 
-char *get_next_line(int fd)
-{
+// char *get_next_line(int fd)
+// {
      
+// }
+
+int main()
+{
+    int fd;
+    char *buff = NULL;
+    fd = open("hello.txt", O_RDONLY);
+    buff = read_file(fd, buff);
+    printf("%s", buff);
+    close(fd);
+    free (buff);
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,10 +6,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#ifndef BUFF_SIZE
+#define BUFF_SIZE 42
+#endif
+
  void	*ft_calloc(size_t nmemb, size_t size);
  int	ft_strlen(const char *s);
  void	*ft_bzero(void *s, size_t n);
  char	*ft_strchr(const char *s, int c);
+ char	*ft_strjoin(char const *s1, char const *s2);
 //char *get_next_line(int fd);
 
 #endif
